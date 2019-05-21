7c3ef7e1e23c5b076a4ec3d10c8900cdf6ebbc55
Mihir Soni <mihson@amazon.com>
2019-05-20 18:36:23 -0700

    Improved diff logic and output

--- commands/diff.go
+++ commands/diff.go
@@ -7,7 +7,10 @@ import (
 	"../destination"
 	"../monitor"
 	mapset "github.com/deckarep/golang-set"
+	"github.com/fatih/color"
+	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
 var diff = &cobra.Command{
@@ -15,6 +18,18 @@ var diff = &cobra.Command{
 	Short: "difference between local and remote monitors",
 	Long:  `this will show print diff between local and remote monitors.`,
 	Run:   showDiff,
+}
+var dmp = diffmatchpatch.New()
+
+func isMonitorChanged(localMonitor monitor.Monitor, remoteMonitor monitor.Monitor) bool {
+	localYaml, _ := yaml.Marshal(localMonitor)
+	remoteYml, _ := yaml.Marshal(remoteMonitor)
+	diffs := dmp.DiffMain(string(remoteYml), string(localYaml), true)
+	fmt.Println("diffs", len(diffs))
+	if len(diffs) > 1 {
+		return true
+	}
+	return false
 }
 
 func showDiff(cmd *cobra.Command, args []string) {
@@ -42,31 +57,40 @@ func showDiff(cmd *cobra.Command, args []string) {
 	changedMonitors := mapset.NewSet()
 	allCommonMonitorsIt := allCommonMonitors.Iterator()
 	for commonMonitor := range allCommonMonitorsIt.C {
-		if isMonitorChanged(localMonitors[commonMonitor.(string)], allRemoteMonitors[commonMonitor.(string)]) != true {
+		monitorName := commonMonitor.(string)
+		if isMonitorChanged(localMonitors[monitorName], allRemoteMonitors[monitorName]) == true {
 			changedMonitors.Add(commonMonitor)
 		}
 	}
-	fmt.Println("monitors to be updated", changedMonitors)
-	// for monitorToBeUpdated := range changedMonitors.Iterator().C {
-	// 	monitorName := monitorToBeUpdated.(string)
-	// 	localYaml, err := yaml.Marshal(localMonitors[monitorName])
-	// 	remoteYml, err := yaml.Marshal(allRemoteMonitors[monitorName])
-	// 	fmt.Println(string(remoteYml))
-	// 	fmt.Println("-------------------------------")
-	// 	fmt.Println(string(localYaml))
-	// 	fmt.Println("-------------------------------")
+	//All New Monitors
+	if allNewMonitors.Cardinality() > 0 {
+		fmt.Println("---------------------------------------------------------")
+		fmt.Println(" These monitors are currently missing in alerting ")
+		fmt.Println("---------------------------------------------------------")
+		for newMonitor := range allNewMonitors.Iterator().C {
+			monitorName := newMonitor.(string)
+			localYaml, _ := yaml.Marshal(localMonitors[monitorName])
+			color.Green(string(localYaml))
+		}
+	}
 
-	// 	if err != nil {
-	// 		fmt.Printf("Unable to convert into YML")
-	// 		os.Exit(1)
-	// 	}
-	// 	dmp := diffmatchpatch.New()
-
-	// 	diffs := dmp.DiffMain(string(remoteYml), string(localYaml), false)
-	// 	fmt.Println(dmp.DiffPrettyText(diffs))
-	// 	// diff := cmp.Diff(allRemoteMonitors[monitorName], localMonitors[monitorName], cmpopts.IgnoreUnexported(monitor.Monitor{}))
-	// 	// fmt.Println(string(diff))
-	// }
+	if changedMonitors.Cardinality() > 0 {
+		fmt.Println("---------------------------------------------------------")
+		fmt.Println(" These are existing monitors, which have been modified ")
+		fmt.Println("---------------------------------------------------------")
+		for monitorToBeUpdated := range changedMonitors.Iterator().C {
+			monitorName := monitorToBeUpdated.(string)
+			localYaml, err := yaml.Marshal(localMonitors[monitorName])
+			remoteYml, err := yaml.Marshal(allRemoteMonitors[monitorName])
+			if err != nil {
+				fmt.Printf("Unable to convert into YML")
+				os.Exit(1)
+			}
+			diffs := dmp.DiffMain(string(remoteYml), string(localYaml), true)
+			diffs = dmp.DiffCleanupSemantic(diffs)
+			fmt.Println(dmp.DiffPrettyText(diffs))
+		}
+	}
 }
 
 func init() {
--- commands/utils.go
+++ commands/utils.go
@@ -1,11 +1,8 @@
 package commands
 
 import (
-	"fmt"
 	"net/url"
-	"reflect"
 
-	"../monitor"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -13,14 +10,6 @@ import (
 func isIDKey(p cmp.Path) bool {
 	step := p[len(p)-1].String()
 	return step == ".ID"
-}
-
-func isMonitorChanged(localMonitor monitor.Monitor, remoteMonitor monitor.Monitor) bool {
-	// diff := cmp.Diff(remoteMonitor, localMonitor, cmpopts.IgnoreUnexported(monitor.Monitor{}), cmp.FilterPath(isIDKey, cmp.Ignore()))
-	// fmt.Println(string(diff))
-	// return cmp.Equal(localMonitor, remoteMonitor, cmpopts.IgnoreUnexported(monitor.Monitor{}), cmp.FilterPath(isIDKey, cmp.Ignore()))
-	fmt.Println("reflect.DeepEqual(remoteMonitor, localMonitor)", reflect.DeepEqual(remoteMonitor, localMonitor))
-	return reflect.DeepEqual(remoteMonitor, localMonitor)
 }
 
 func IsURL(str string) bool {
