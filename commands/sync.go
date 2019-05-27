9f524839bfdccf28b1368607c039300c01bee8dd
Mihir Soni <mihson@amazon.com>
2019-05-27 02:22:01 -0700

    Enable syncing remote monitors to lcoal yaml file

--- commands/sync.go
+++ commands/sync.go
@@ -5,11 +5,13 @@ import (
 	"path/filepath"
 
 	"../destination"
+	"../monitor"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
 var syncDestinatons bool
+var syncMonitors bool
 var sync = &cobra.Command{
 	Use:   "sync [Flags]",
 	Short: "sync operation",
@@ -18,10 +20,14 @@ var sync = &cobra.Command{
 }
 
 func runSync(cmd *cobra.Command, args []string) {
+	destinations, err := destination.GetRemote(esClient)
+	check(err)
 	if syncDestinatons {
-		destinations, err := destination.GetRemote(esClient)
-		check(err)
 		writeDestinations(destinations)
+	} else if syncMonitors {
+		monitors, _, err := monitor.GetAllRemote(esClient, destinations)
+		check(err)
+		writeMonitors(monitors)
 	}
 }
 
@@ -39,7 +45,26 @@ func writeDestinations(destinations map[string]string) {
 	file.Write(data)
 }
 
+func writeMonitors(monitors map[string]monitor.Monitor) {
+	destinationsPath := filepath.Join(rootDir, "monitors.yaml")
+	if _, err := os.Stat(destinationsPath); os.IsNotExist(err) {
+		_, err = os.Create(destinationsPath)
+		check(err)
+	}
+	file, err := os.OpenFile(destinationsPath, os.O_WRONLY, 0644)
+	check(err)
+	defer file.Close()
+	var monitorsList []monitor.Monitor
+	for name := range monitors {
+		monitorsList = append(monitorsList, monitors[name])
+	}
+	data, err := yaml.Marshal(monitorsList)
+	check(err)
+	file.Write(data)
+}
+
 func init() {
 	sync.Flags().BoolVarP(&syncDestinatons, "destinations", "d", false, "sync all destinations from ES and write destinations.yml file")
+	sync.Flags().BoolVarP(&syncMonitors, "monitors", "m", false, "sync all monitors from ES and write monitors.yml helpful to hav start of from your existing monitors")
 	rootCmd.AddCommand(sync)
 }
